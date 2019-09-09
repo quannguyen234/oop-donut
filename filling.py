b96326b2b0a89bfe33a0ad7790b678491b105486
Quan Nguyen <quannguyen@Quans-MacBook-Pro.local>
2019-09-08 21:49:47 -0400

    add filling, glaze, ultimate_donut

---
+++ filling.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 16:47:43 2019
+
+@author: quannguyen
+"""
+#style= ["Classic", "Fruity", "Explorer"]
+#sweetness = [0,1,2]
+class Filling:
+    
+    
+    def __init__(self, name, style, sweetness, fatness, allergy = False):
+        self.name = name
+        self.style = style
+        self.sweetness = sweetness
+        self.fatness = fatness
+        self.allergy = allergy
+        
+    
+        
+        
+        
+    
+        
+        
+       
---
+++ glaze.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  7 16:50:49 2019
+
+@author: quannguyen
+"""
+ 
+from filling import Filling
+
+class Glaze:
+    
+    def __init__(self, name, sweetness, fatness):
+        self.name = name
+        self.sweetness = sweetness
+        self.fatness = fatness
+        
+        
